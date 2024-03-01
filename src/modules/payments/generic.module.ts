e60deb4c2b48624551f049e4ea149cdee26ca087
@chicoOmicrom <jblanco@lya-electronic.com>
2024-03-01 15:17:34 -0500

    feat: modifications skeleton template

--- src/modules/payments/generic.module.ts
+++ src/modules/payments/generic.module.ts
@@ -3,13 +3,19 @@ import { GenericController } from './genericController.controller';
 import { GenericService } from './generic.service';
 import { GenericMiddleware } from './middleware/epayco';
 import { ImplementationsModules } from 'src/dataAccess/implementations/implementations.module';
+import { DatabaseModule } from 'src/dataAccess/databases';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    DatabaseModule,
     ImplementationsModules,
+    ConfigModule
   ],
   controllers: [GenericController],
-  providers: [GenericService],
+  providers: [
+    //repositories
+    GenericService],
   exports: [GenericService],
 })
 export class GenericModule implements NestModule {
